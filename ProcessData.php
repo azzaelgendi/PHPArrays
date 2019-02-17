8802813b42e471b0fda070c61809bd0973561f1d
Azza Elgendy <37027634+azzaelgendi@users.noreply.github.com>
2019-02-17 18:53:11 -0500

    Update ProcessData.php

--- ProcessData.php
+++ ProcessData.php
@@ -3,7 +3,7 @@
     <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>InClass8 Azza Elgendy</title>
+            <title>processData page</title>
             <link rel="stylesheet" href="main.css" />
     </head>
     <body>
